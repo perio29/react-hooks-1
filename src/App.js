5e62812f9944bd1bdd00f14cbbf26ca53f85a2cb
perio29 <tokuzou10yohji@gmail.com>
2021-09-16 09:59:46 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-const App = (props) => {
-  const [state, setState] = useState(props);
-  const { name, price } = state;
-
-  useEffect(() => {
-    console.log("This is like componentDidMount or componentDidUpdate.");
-  });
-
-  useEffect(() => {
-    console.log("This is like componentDidMount.");
-  }, []);
-
-  useEffect(() => {
-    console.log("This callback is for name only.");
-  }, [name]);
-
-  return (
-    <>
-      <p>
-        現在の{name}は{price}円です。
-      </p>
-      <button onClick={() => setState({ ...state, price: price + 1 })}>
-        +1
-      </button>
-      <button onClick={() => setState({ ...state, price: price - 1 })}>
-        -1
-      </button>
-      <button onClick={() => setState(props)}>Reset</button>
-      <input
-        value={name}
-        onChange={(e) => setState({ ...state, name: e.target.value })}
-      ></input>
-    </>
-  );
-};
-
-App.defaultProps = {
-  name: "",
-  price: 1000,
+const App = () => {
+  return <>This is a template for React app</>;
 };
 
 export default App;
